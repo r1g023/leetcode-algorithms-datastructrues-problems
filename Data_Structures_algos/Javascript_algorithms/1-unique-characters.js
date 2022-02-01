407079d5aa6a433cf0d870df4acc2590b41001f5
rigo0523 <limon1285@gmail.com>
2022-01-31 23:01:42 -0600

    is unique problem with sorted

---
+++ Data_Structures_algos/Javascript_algorithms/1-unique-characters.js
@@ -0,0 +1,55 @@
+// var listOfCities = [ "nashville", "nashville", "los angeles", "nashville", "memphis", "barcelona", "sevillia", "isla canary", "barcelona", ];
+// //FIRST METHOD/
+// // return a count of the distinct duplicates ( not the times an entry is repeated ) entries in the list above. You can run your code by pressing the aqua colored "Run" button above left.
+// function countDuplicatesInList(cityList) {
+//   var count = 0;
+//   // fill in code here
+//   var dupCities = {};
+//   var arrCount;
+//   for (let i = 0; i < cityList.length; i++) {
+//     dupCities[cityList[i]] = dupCities[cityList[i]] + 1 || 1;
+//     arrCount = Object.values(dupCities); // get values of object and turn into array
+//   }
+
+//   arrCount.forEach((item) => { // loop through array and get items greater than 1 and add to count
+//     if (item > 1) {
+//       count = count + 1;
+//     }
+//   });
+//   console.log(dupCities, arrCount); // { nashville: 3, 'los angeles': 1, memphis: 1, barcelona: 2, sevillia: 1, 'isla canary': 1 }
+//   return count; // 2
+// }
+
+// function main() {
+//   var count = countDuplicatesInList(listOfCities);
+//   if (count == 2) {
+//     console.log("yep!");
+//   } else {
+//     console.log("failed :( count = " + count);
+//   }
+// }
+
+// main(); // failed :( count = 3
+// //time and space complexit of countDuplicatesInList is O(n)
+
+//create a function that determines wether all characters in a string are uique or not. Make it case-sensitive, meaning a string with both 'a' and 'A' could pass the test.
+
+//Input: String
+//Output: Boolean
+function isUnique(str) {
+  for (let i = 0; i < str.length; i++) {
+    // O(n)
+    if (str.lastIndexOf(str[i]) !== i) {
+      // O(n)
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(isUnique("abcde"));
+// * Time O(n * n) -> O(n^2)
+// * Space O(1)
+// ? What is the time complexity of this algorithm?
+
+//isUnique('89%df#s*a&x'), isUnique('abcadef'), isUnique('isuniqque')
