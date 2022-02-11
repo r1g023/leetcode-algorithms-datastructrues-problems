84f6498abbc6bcccc9fe5e1cf301841a83f748a4
rigo0523 <limon1285@gmail.com>
2022-02-11 16:53:30 -0600

    promises review

--- Data_Structures_algos/javascript_challenges/js-sync-async.js
+++ Data_Structures_algos/javascript_challenges/js-sync-async.js
@@ -6,6 +6,10 @@
 
 // 1. Browser APIs / Web APIs
 // 2. Promises
+// 3. JAvaScript is synchronous by default
+// 4. Async => operations are very normal using browser APIS, event handelers, promises
+// 5. callbacks are great notification mechanism for
+// 6. Promises with its states, values are on excellent way to handle async alls.
 
 function f1() {
   console.log("f1");
@@ -21,6 +25,7 @@ function f2() {
 function f3() {
   console.log("f3 stack"); // runs first
   setTimeout(() => {
+    // web api stack
     console.log("f2 stack"); //runs f2() stack
     f2();
   }, 4000);
@@ -28,4 +33,57 @@ function f3() {
 
 f3();
 
-new Promise(function (resolve, reject) {});
+// Promises
+// Promise is an object that represents a value that may not be available yet.
+const promise = new Promise(function (resolve, reject) {
+  resolve(`I'm a resolved\n promise`); // if promise is not resolved, reject() is called
+  reject(new Error("Error, can't fetch"));
+});
+
+promise
+  .then((result) => {
+    console.log("RESULTT--->", result);
+  })
+  .catch((error) => {
+    console.log("error--->", error);
+  });
+
+function executePromise() {
+  promise
+    .then((result) => {
+      console.log("RESULTT--->", result);
+    })
+    .catch((error) => {
+      console.log("error--->", error);
+    });
+}
+
+// executePromise();
+// Promises
+// Promise is an object that represents a value that may not be available yet.
+
+// ? PROMISE MORE ADVANCED METHODS
+let getUser = new Promise(function (resolve, reject) {
+  const user = {
+    name: "John",
+    email: "james@example.com",
+    password: "password",
+    permission: ["db", "admin", "user"],
+  };
+  resolve(user);
+});
+
+getUser
+  .then((response) => {
+    console.log(response);
+    if (response.permission.includes("users")) {
+      throw new Error("you are not allowed to see this");
+    }
+    return response;
+  })
+  .then((email) => {
+    console.log(email.email);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
