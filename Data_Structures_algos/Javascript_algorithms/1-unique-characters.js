534002eaa279fd0afd1ea474fa7bc68d1a77a0d2
LimRi <57161327+rigo0523@users.noreply.github.com>
2022-03-02 23:54:17 -0600

    find duplicates and unique chars

--- Data_Structures_algos/Javascript_algorithms/1-unique-characters.js
+++ Data_Structures_algos/Javascript_algorithms/1-unique-characters.js
@@ -2,55 +2,54 @@
 
 // ? FIRST METHOD  BASED ON A COMPANY CODING CHALLENGE - MY SOLUTION
 // return a count of the distinct duplicates ( not the times an entry is repeated ) entries in the list above. You can run your code by pressing the aqua colored "Run" button above left.
-// function countDuplicatesInList(cityList) {
-//   var count = 0;
+function countDuplicatesInList(cityList) {
+   var count = 0;
 // fill in code here
-//   var dupCities = {};
-//   var arrCount;
-//   for (let i = 0; i < cityList.length; i++) {
-//     dupCities[cityList[i]] = dupCities[cityList[i]] + 1 || 1;
-//     arrCount = Object.values(dupCities); // get values of object and turn into array
-//   }
+   var dupCities = {};
+   var arrCount;
+   for (let i = 0; i < cityList.length; i++) {
+     dupCities[cityList[i]] = dupCities[cityList[i]] + 1 || 1;
+     arrCount = Object.values(dupCities); // get values of object and turn into array
+   }
 
-//   arrCount.forEach((item) => { // loop through array and get items greater than 1 and add to count
-//     if (item > 1) {
-//       count = count + 1;
-//     }
-//   });
-//   console.log(dupCities, arrCount); // { nashville: 3, 'los angeles': 1, memphis: 1, barcelona: 2, sevillia: 1, 'isla canary': 1 }
-//   return count; // 2
-// }
+   arrCount.forEach((item) => { // loop through array and get items greater than 1 and add to count
+     if (item > 1) {
+       count = count + 1;
+     }
+   });
+   console.log(dupCities, arrCount); // { nashville: 3, 'los angeles': 1, memphis: 1, barcelona: 2, sevillia: 1, 'isla canary': 1 }
+   return count; // 2
+ }
 
-// function main() {
-//   var count = countDuplicatesInList(listOfCities);
-//   if (count == 2) {
-//     console.log("yep!");
-//   } else {
-//     console.log("failed :( count = " + count);
-//   }
-// }
+ function main() {
+   var count = countDuplicatesInList(listOfCities);
+   if (count == 2) {
+     console.log("yep!");
+   } else {
+     console.log("failed :( count = " + count);
+   }
+ }
 
-// main(); // failed :( count = 3
+ main(); // failed :( count = 3
 // * time and space complexit of countDuplicatesInList is O(n)
 
 //create a function that determines wether all characters in a string are uique or not. Make it case-sensitive, meaning a string with both 'a' and 'A' could pass the test.
-
 //Input: String
 //Output: Boolean
 
 // ? SECOND Method with lastIndexOf method
-// function isUnique(str) {
-//   for (let i = 0; i < str.length; i++) {
+ function isUnique(str) {
+   for (let i = 0; i < str.length; i++) {
+ O(n)
+     if (str.lastIndexOf(str[i]) !== i) {
 // O(n)
-//     if (str.lastIndexOf(str[i]) !== i) {
-// O(n)
-//       return false;
-//     }
-//   }
-//   return true;
-// }
+       return false;
+     }
+   }
+   return true;
+ }
 
-// console.log(isUnique("abcde"));
+ console.log(isUnique("abcde"));
 // * Time O(n * n) -> O(n^2)
 // * Space O(1)
 
