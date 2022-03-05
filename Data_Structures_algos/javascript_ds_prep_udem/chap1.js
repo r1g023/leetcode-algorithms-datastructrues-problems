5b48bafd360f1eefe1bb22a2281cfb2a5fe7de63
rigo0523 <limon1285@gmail.com>
2022-03-05 00:19:51 -0600

    installed jest to simulate leetcode enviroment

---
+++ Data_Structures_algos/javascript_ds_prep_udem/chap1.js
@@ -0,0 +1,15 @@
+//loop sample
+for (let i = 0; i < 5; i++) {
+  //console.log(`running loop ${i}`);
+}
+
+// while loop example
+let age = 18;
+while (age < 18) {
+  // console.log(`age is ${age}`);
+  // age++;
+}
+
+age < 30
+  ? console.log(`younger than ${age}`)
+  : console.log(`older than ${age}`);
