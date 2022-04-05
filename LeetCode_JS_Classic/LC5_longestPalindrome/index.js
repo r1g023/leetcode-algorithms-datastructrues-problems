8344f7706bc7ffa52ee891f4b2eaa2f4b6fab937
rigo0523 <limon1285@gmail.com>
2022-04-04 23:20:17 -0500

    third practice over longest palindrome

--- LeetCode_JS_Classic/LC5_longestPalindrome/index.js
+++ LeetCode_JS_Classic/LC5_longestPalindrome/index.js
@@ -13,43 +13,70 @@
 // Input: s = "cbbd"
 // Output: "bb"
 
-function longestPalindrome(s) {
+const longestPalindrome = (s) => {
+  if (!s || s.length <= 1) return s;
   let startIndex = 0;
   let maxLength = 1;
-
-  //function expandAroundMiddle
   function expandAroundMiddle(left, right) {
     while (left >= 0 && right < s.length && s[left] === s[right]) {
-      console.log(s.length);
-      console.log(left, s[left]);
-      console.log(right, s[right]);
-      //code only runs if we have a valid palindrome
-      const currentPalLength = right - left + 1; //array indece starts at 0, add 1 to compensate
-      console.log(right);
-      console.log(left);
-      console.log(right - left + 1);
+      const currentPalLength = right - left + 1;
+
+      console.log(currentPalLength);
       if (currentPalLength > maxLength) {
         maxLength = currentPalLength;
         console.log(maxLength);
         startIndex = left;
-        console.log(left);
+        console.log(startIndex);
       }
+
+      console.log(left, s[left], right, s[right]);
       left--;
       right++;
     }
   }
 
-  //iterate through every character on input string
   for (let i = 0; i < s.length; i++) {
-    console.log("i", i);
-
     expandAroundMiddle(i - 1, i + 1);
-    expandAroundMiddle(i, i + 1); // accounts for spaces in between characters
+    expandAroundMiddle(i, i + 1);
   }
-
   return s.slice(startIndex, startIndex + maxLength);
-}
+};
 
-longestPalindrome("-racecar_");
+longestPalindrome("yanaz");
 
 module.exports = longestPalindrome;
+
+// let startIndex = 0;
+// let maxLength = 1;
+
+// //function expandAroundMiddle
+// function expandAroundMiddle(left, right) {
+//   while (left >= 0 && right < s.length && s[left] === s[right]) {
+//     console.log(s.length);
+//     console.log(left, s[left]);
+//     console.log(right, s[right]);
+//     //code only runs if we have a valid palindrome
+//     const currentPalLength = right - left + 1; //array indece starts at 0, add 1 to compensate
+//     console.log(right);
+//     console.log(left);
+//     console.log(right - left + 1);
+//     if (currentPalLength > maxLength) {
+//       maxLength = currentPalLength;
+//       console.log(maxLength);
+//       startIndex = left;
+//       console.log(left);
+//     }
+//     left--;
+//     right++;
+//   }
+// }
+
+// //iterate through every character on input string
+// for (let i = 0; i < s.length; i++) {
+//   console.log("i", i);
+
+//   expandAroundMiddle(i - 1, i + 1);
+//   expandAroundMiddle(i, i + 1); // accounts for spaces in between characters
+// }
+
+// return s.slice(startIndex, startIndex + maxLength);
