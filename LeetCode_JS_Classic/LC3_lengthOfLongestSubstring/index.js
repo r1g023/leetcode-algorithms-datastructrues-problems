44a0167d408f4520141bd4abf8b894841bb1593b
rigo0523 <limon1285@gmail.com>
2022-04-07 23:09:05 -0500

    refine lengthOfLongestSubstring

--- LeetCode_JS_Classic/LC3_lengthOfLongestSubstring/index.js
+++ LeetCode_JS_Classic/LC3_lengthOfLongestSubstring/index.js
@@ -5,21 +5,37 @@
 // lengthOfLongestSubstring("bbbbb") --> 1 since length of "b"
 
 function lengthOfLongestSubstring(s) {
-  if(s.length <= 1) return s.length;
-    
-    const seen = new Map();
-    let left = 0, longest = 0;
-    
-    for( let right = 0; right < s.length; right++){
-        const curr = s[right];
-        const pre = seen.get(curr);
-        if(pre >= left){
-            left = pre+1;
-        }
-        seen.set(curr,right);
-        longest = Math.max(longest, (right - left + 1));
+  let windowCharsMap = {};
+  let windowStart = 0;
+  let maxLength = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    const endChar = s[i];
+    if (windowCharsMap[endChar] >= windowStart) {
+      windowStart = windowCharsMap[endChar] + 1;
     }
-    return longest;
+    windowCharsMap[endChar] = i;
+    maxLength = Math.max(maxLength, i - windowStart + 1);
+  }
+  return maxLength;
 }
 
 module.exports = lengthOfLongestSubstring;
+
+// if (s.length <= 1) return s.length;
+
+//
+// const seen = new Map();
+// let left = 0,
+//   longest = 0;
+
+// for (let right = 0; right < s.length; right++) {
+//   const curr = s[right];
+//   const pre = seen.get(curr);
+//   if (pre >= left) {
+//     left = pre + 1;
+//   }
+//   seen.set(curr, right);
+//   longest = Math.max(longest, right - left + 1);
+// }
+// return longest;
