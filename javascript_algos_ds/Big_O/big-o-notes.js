4c3c7d2147a3b68163ed423277773e9a3a77fd3e
rigo0523 <limon1285@gmail.com>
2022-04-12 22:40:31 -0500

    big o quiz

--- javascript_algos_ds/Big_O/big-o-notes.js
+++ javascript_algos_ds/Big_O/big-o-notes.js
@@ -1,84 +1,93 @@
 //When dealing with big o notation
-//1) OMEGA  -
-//2) THETA
+//1) OMEGA  - Ω
+//2) THETA - Θ
 //3) OMNICRON - BIG O
+
+// 1) Complexity in terms of input size, N
+// 2) machine-independent complexity
+// 3) basic computer steps
+// 4) time and space
+
+// Time complexity refers to the average time an algorithm takes as a function of its inputs, not the time required for a specific instance. If you are lucky to find the element you seek in the first step, that’s fine. But it’s not indicative of the average or worst-case time, which is what most people are interested in.
+
+// worst-case, best-case, average-case
 
 // ? WORST CASE S
 //Big O is the one that's seen more often,
 let array = [1, 2, 3, 4, 5]; // look for NUMBER 1, iterate with a for loop
-//---Best cASE would be #1, least iterations = OMEGA SYMBOL
-//---WORST CASE = 7, THETA SYMBOL
-//---AVERAGE CASE = 4, OMNICROM OR BIG O
+//---Best cASE would be #1, least iterations = OMEGA SYMBOL Ω
+//---WORST CASE = BIG O omnicorm
+//---AVERAGE CASE = 4 THETA, MIDDLE
 //When we measure BIG-O, We are always measuring worst case
 
 //-----------------------------------------------BIG O: O(n)------------------------------------------------//
-// function logItems(n) {
-//   for (let i = 0; i < n; i++) {
-//     console.log(i);
-//   }
-// } //O(n)
+function logItems(n) {
+  for (let i = 0; i < n; i++) {
+    console.log(i);
+  }
+} //O(n)
 
-// logItems(5); // O(n) operation
+logItems(5); // O(n) operation
 //ON number of operations will always be proportional to whatever N is.
 
 //-----------------------------------------------BIG O: Drop Constants ------------------------------------------------//
-// function logItems(n) {
-//   for (let i = 0; i < n; i++) {
-//     console.log(i);
-//   }
+function logItems(n) {
+  for (let i = 0; i < n; i++) {
+    console.log(i);
+  }
 
-//   for (let j = 0; j < n; j++) {
-//     console.log(j);
-//   }
-// }
+  for (let j = 0; j < n; j++) {
+    console.log(j);
+  }
+} // DROP CONSTANTS
 
-// logItems(5); // n + n = 2n = O(2n), it doesn't matter if it's 0(100n), it's always constant, we drop the remaining Ns
+logItems(5); // n + n = O(2n) = O(n), it doesn't matter if it's 0(100n), it's always constant, we drop the remaining Ns
 
 //-----------------------------------------------BIG O: O(n^2) ------------------------------------------------//
-// function logItems(n) {
-//   for (let i = 0; i < n; i++) {
-//     for (let j = 0; j < n; j++) {
-//       for (let k = 0; k < n; k++) {
-//         console.log(i, j, k);
-//       }
-//     }
-//   }
-// } // O(n^3) = o(n^2)
+function logItems(n) {
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      for (let k = 0; k < n; k++) {
+        console.log(i, j, k);
+      }
+    }
+  }
+} // O(n^3) = o(n^2)
 
-// logItems(3);
+logItems(3); // DOESN'T matter if it's n^100, we always write it as O(n^2)
 
 //-----------------------------------------------BIG O: Drop Non-Dominants ------------------------------------------------//
-// function logItems(n) {
-//   for (let i = 0; i < n; i++) {
-//     for (let j = 0; j < n; j++) {
-//       console.log("i:", i, "j:", j);
-//     }
-//   } // O (n^2)
-//   for (let k = 0; k < n; k++) {
-//     console.log(k);
-//   } // O (n)
-// }
-//Time and Space: O(n^2 + n)
-//n is not really affecting the number of operations because n2 is the dominant term and N by itself is the non-dominant term.
+function logItems(n) {
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      console.log("i:", i, "j:", j);
+    }
+  } // O (n^2)
+  for (let k = 0; k < n; k++) {
+    console.log(k);
+  } // O (n)
+}
+// Time and Space: O(n^2 + n)
+// n is not really affecting the number of operations because n2 is the dominant term and N by itself is the non-dominant term.
 
-// logItems(3);
+logItems(3);
 
 //-----------------------------------------------BIG O: O(1) ------------------------------------------------//
 
-// function addItems(n) {
-//   return n + n + n; // = o(2) = o(1)
-//   //number of operations does not change as N  changes
-//   //it's the most efficient big O
-// }
+function addItems(n) {
+  return n + n + n; // = o(2) = o(1)
+  //number of operations does not change as N  changes
+  //it's the most efficient big O
+}
 
-// // O(1) is often referred to as constant time, number of operations will always be 1,
+// O(1) is often referred to as constant time, number of operations will always be 1,
 
-// addItems(2);
+addItems(2);
 
 //-----------------------------------------------BIG O: O(log n) ------------------------------------------------//
 //array from 1 to 8
 let array1 = [1, 2, 3, 4, 5, 6, 7, 8];
-//find 1, what would be the most efficient way to find it, cut aray in half, is the 1 on first half or second half.
+//find 1, what would be the most efficient way to find it, cut array in half, is the 1 on first half or second half.
 // 1 is on the first half [1,2,3,4]
 // divide array again, 1 is on the first half of [1,2,3,4]
 // 1 is on the first half [1,2]
@@ -90,43 +99,43 @@ let array1 = [1, 2, 3, 4, 5, 6, 7, 8];
 // O(nlog n) is used in some sorting algorithms, most efficient that you can make a sorting algo, unless you're sorting only numbers, but if you're sorting data like strings, this is the most efficient way.
 
 //-----------------------------------------------BIG O: different terms for Inputs ------------------------------------------------//
-// function logItems(n) {
-//   for (let i = 0; i < n; i++) {
-//     console.log(i);
-//   }
-//   for (let k = 0; k < n; k++) {
-//     console.log(k);
-//   }
-// }
-// // T & S: O(2n), drop the constant 2, so it becomes linear O(n)
+function logItems(n) {
+  for (let i = 0; i < n; i++) {
+    console.log(i);
+  }
+  for (let k = 0; k < n; k++) {
+    console.log(k);
+  }
+}
+// T & S: O(2n), drop the constant 2, so it becomes linear O(n)
 
-// function logItems(a, b) {
-//   for (let i = 0; i < a; i++) {
-//     // O(a)
-//     console.log(i);
-//   } // first input of A will run A times (3 times)
-//   for (let j = 0; j < b; j++) {
-//     // O(b)
-//     console.log(j);
-//     // second input of B will run B times (4 times)
-//   }
+function logItems(a, b) {
+  for (let i = 0; i < a; i++) {
+    // O(a)
+    console.log(i);
+  } // first input of A will run A times (3 times)
+  for (let j = 0; j < b; j++) {
+    // O(b)
+    console.log(j);
+    // second input of B will run B times (4 times)
+  }
 
-//   // O(a)+O(b) = O (a + b)
-// }
+  // O(a)+O(b) = O (a + b)
+}
 
-// logItems(3, 4);
+logItems(3, 4);
 
-// function logItems(a, b) {
-//   //nested for loop
-//   for (let i = 0; i < a; i++) {
-//     for (let j = 0; j < b; j++) {
-//       console.log(i, j);
-//     }
-//   }
-//   // O (a * b), multiply when inputs are different and are nested,
-// }
+function logItems(a, b) {
+  //nested for loop
+  for (let i = 0; i < a; i++) {
+    for (let j = 0; j < b; j++) {
+      console.log(i, j);
+    }
+  }
+  // O (a * b), multiply when inputs are different and are nested,
+}
 
-// logItems(2, 3);
+logItems(2, 3);
 
 //-----------------------------------------------BIG O: Arrays ------------------------------------------------//
 
@@ -145,3 +154,13 @@ myArray.splice(1, 0, "Hi"); // at index 1, remove 0 items, but add char 'hi' [ 1
 console.log(myArray);
 
 //-----------------------------------------------BIG O: Wrap up ------------------------------------------------//
+n = 100;
+// o(1)=1  it's constant, N will always equal to O(1)
+// O(log n) = 7, approximately 7 if n is 100, divide and conquer
+// o(n) = 100,it's linear or proportional, obviously it's 100 items
+// o(n^2) = 10,000 loop within a loop
+n = 1000;
+// o(1)=1  it's constant, N will always equal to O(1)
+// O(log n) = 10, approximately 2 ^ 10 = 1024, around there
+// o(n) = 1000,it's linear or proportional, obviously it's 1000 items
+// o(n^2) = 10,000,000 loop within a loop, grows very fast
