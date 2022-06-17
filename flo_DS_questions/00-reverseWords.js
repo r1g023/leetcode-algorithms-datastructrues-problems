9894dd42120b4a72791c5e427b41775561848e5c
rigo0523 <limon1285@gmail.com>
2022-06-17 16:36:18 -0500

    reverse words algo

---
+++ flo_DS_questions/00-reverseWords.js
@@ -0,0 +1,14 @@
+// Given a string of words, write a function that reverses all of the words within the string.
+
+// Example:
+// $ reverse("The greatest victory is that which requires no battle")
+// $ "battle no requires which that is victory greatest The"
+
+function reverseWords(string) {
+  return string.split(" ").reverse().join(" ");
+}
+
+console.log(
+  reverseWords("The greatest victory is that which requires no battle")
+);
+// "battle no requires which that is victory greatest The"
