d72ceef33927a849746888da0cfdf7fb951db50d
rigo0523 <limon1285@gmail.com>
2022-06-23 22:57:14 -0500

    longTextGenerator

---
+++ flo_DS_questions/01_longTextGenerator.js
@@ -0,0 +1,22 @@
+// Create a loooong teeeext generator that takes in a string and an integer n, and multiplies the vowels in the string by n.
+// The vowels are a, e, i, o, u.
+// Example:
+function longTextGenerator(string, n) {
+  let vowels = ["a", "e", "i", "o", "u"];
+  let newString = "";
+  for (let i = 0; i < string.length; i++) {
+    if (vowels.includes(string[i])) {
+      console.log(string[i]); // e, o
+      let getVowels = string[i].repeat(n);
+      console.log(getVowels); // ee, oo
+      newString = newString + getVowels;
+    } else {
+      console.log(string[i]); // h, l, l
+      newString = newString + string[i];
+    }
+  }
+  console.log(newString); // heelloo
+  return newString;
+}
+
+console.log(longTextGenerator("hello", 2));
