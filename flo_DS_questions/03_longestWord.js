9551a872b33c6da91770ec05b25284f9cc90963c
rigo0523 <limon1285@gmail.com>
2022-07-02 14:44:16 -0500

    readding longestWord problem

--- flo_DS_questions/03_longestWord.js
+++ flo_DS_questions/03_longestWord.js
@@ -1,19 +1,24 @@
-// Given a string and a set of words in an array, find the longest word in the array that is a subsequence of string.
-// string = abppplee
-// words = ["able", "ale", "apple", "bale", "kangaroo"]
-// Output: apple
-// create a function that returns the longest word in the array that is a subsequence of string.
+// Given a string str and a set of words dict, find the longest word in dict that is a subsequence of str.
+// Example:
+// let str = "abppplee"
+// let dict = {"able", "ale", "apple", "bale", "kangaroo"}
+
+// $ longestWord(str, dict)
+// $ 'apple'
+// "able" and "ale" also work, but are shorter than "apple"
+// "bale" has all the right letters, but not in the right order
 function longestWord(str, dict) {
   let longest = "";
   let current = "";
   for (let i = 0; i < str.length; i++) {
-    current += str[i];
+    current = current + str[i];
     console.log(current); // abppplee
     // need to check further...
   }
   console.log(longest);
   return longest;
 }
+
 console.log(
   longestWord("abppplee", ["able", "ale", "apple", "bale", "kangaroo"])
 );
